6d3b4ed32e5d359bd9ddd0d72c1887f64978e985
emgriggs <66389050+emgriggs@users.noreply.github.com>
2020-12-14 12:49:29 -0800

    Update run_rais.R

--- code/run_rais.R
+++ code/run_rais.R
@@ -4,4 +4,4 @@
 base_dir <- "/media/spinner/rais/rais_original_files"
 year <- 2012 # or any year
 
-rais_data <- map_dfr(gather_rais(base_dir, year = 2012), ~make_rais(.x, base_dir, year = 2012))
+rais_data <- map_dfr(gather_rais(base_dir, year = 2012), ~make_rais(.x, base_dir, year = 2012, size = Inf))
