74e8dd857c3839b35e8d0b652a3d73bd101994d6
Danny Hidalgo <fdhidalgo@gmail.com>
2020-12-16 10:47:47 -0500

    Reformulated code to not combine states in order to deal with memory issues. Removed extraneous functions

--- code/gather_rais.R
+++
@@ -1,8 +0,0 @@
-### gather_rais ###
-## this function finds all files in a directory and creates a list of them
-
-gather_rais <- function(base_dir, year){
-
-states_list <- as.list(list.files(glue("{base_dir}/{year}")))
-
-}
--- code/run_rais.R
+++ code/run_rais.R
@@ -1,14 +1,27 @@
 ### run_rais ###
 ## assuming all functions have been built and packages run, this file will run the data for you
 source("./code/package_list.R")
-source("./code/gather_rais.R")
-source("./code/make_rais.R")
 source("./code/masterlist_rais.R")
 source("./code/clean_rais_2013.R")
+setDTthreads(1)
 
-base_dir <- "/media/spinner/rais/rais_original_files"
-#base_dir <- "/mnt/danny_data/rais/rais_original_files"
-year <- 2013 # or any year
+clean_save_rais <- function(file, clean_func, out_file){
+  clean_data <- fread(file,
+                      colClasses=c("character")) %>%
+    clean_func()
+  fwrite(clean_data, file = out_file)
+}
 
-rais_data <- map_dfr(gather_rais(base_dir, year = year),
-                     ~make_rais(.x, base_dir, year = year, size = 1000))
+base_dir <- "/media/spinner/rais"
+year <- 2013
+clean_func <- clean_rais_2013
+
+files <- dir(glue(base_dir, "/rais_original_files/", "{year}"), full.names = TRUE)
+out_files <-  paste0(glue(base_dir, "/rais_cleaned/", "{year}", "/"),
+                     gsub(".*/(.*)\\.txt", files, replacement = "\\1"),
+                     ".gz")
+
+for(i in seq_along(1:length(files))){
+  print(i)
+  clean_save_rais(file = files[i], clean_func = clean_func, out_file = out_files[i])
+}
