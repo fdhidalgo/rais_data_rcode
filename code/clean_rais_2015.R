128efa092e380ca71522975726fb83c11cb65daa
emgriggs <66389050+emgriggs@users.noreply.github.com>
2021-01-08 08:48:55 -0800

    Create clean_rais_2015.R

---
+++ code/clean_rais_2015.R
@@ -0,0 +1,38 @@
+clean_rais_2015 <- function(rais_data) {
+  rais_data %>% 
+    as.data.table() %>% 
+    rename_rais_2015() %>% 
+    make_rais_lazy() %>% 
+    destring_rais_2015() %>%
+    trim_rais() %>% 
+    CPF_rais() %>% 
+    identificad_rais() %>% 
+    radiccnpj_rais() %>% 
+    empem3112_rais %>% 
+    tpvinculo_rais() %>% 
+    causadesli_rais() %>% 
+    diadesli_rais() %>% 
+    ocupacao94_rais_2012() %>%
+    ocup2002_rais() %>% 
+    tipoadm_rais() %>% 
+    tiposal_rais() %>% 
+    indalvara_rais %>% 
+    indpat_rais() %>% 
+    indsimples_rais() %>% 
+    grinstrucao_rais() %>% 
+    genero_rais_2012 %>% 
+    nacionalidad_rais() %>% 
+    tpdefic_rais() %>% 
+    raca_cor_rais() %>% 
+    tamestab_rais() %>% 
+    tipoestbl_rais() %>% 
+    commareplace_rais_2015() %>% 
+    natjuridica_rais() %>% 
+    causafast_rais() %>% 
+    diainiafmesiniaf_rais() %>% 
+    dtnascimento_rais() %>% 
+    dtadmissao_rais() %>% 
+    as_tibble() %>% 
+    assertclass_rais() %>%
+    reorder_rais_2014()
+}
