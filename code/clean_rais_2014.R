7049a4236ff291504bcb17aa14dedcc74962ccb7
emgriggs <66389050+emgriggs@users.noreply.github.com>
2021-01-08 07:56:54 -0800

    Update clean_rais_2014.R

--- code/clean_rais_2014.R
+++ code/clean_rais_2014.R
@@ -34,6 +34,6 @@ clean_rais_2014 <- function(rais_data) {
     dtnascimento_rais() %>% 
     dtadmissao_rais() %>% 
     as_tibble() %>% 
-    rais_assertclass() %>%
+    assertclass_rais() %>%
     reorder_rais_2014()
 }
