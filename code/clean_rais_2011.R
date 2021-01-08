e5c20db671e781d73555fa959e3790a67d35ea97
emgriggs <66389050+emgriggs@users.noreply.github.com>
2021-01-08 07:54:07 -0800

    Create clean_rais_2011.R
    file for running 2011

---
+++ code/clean_rais_2011.R
@@ -0,0 +1,37 @@
+clean_rais_2011 <- function(rais_data) {
+  rais_data %>% 
+    as.data.table() %>% 
+    rename_rais_2012() %>% 
+    make_rais_lazy() %>% 
+    destring_rais_2012() %>% 
+    trim_rais() %>% 
+    CPF_rais() %>% 
+    identificad_rais() %>% 
+    radiccnpj_rais() %>% 
+    empem3112_rais %>% 
+    tpvinculo_rais() %>% 
+    causadesli_rais() %>% 
+    ocupacao94_rais_2012() %>%
+    ocup2002_rais() %>% 
+    tipoadm_rais() %>% 
+    tiposal_rais() %>% 
+    indalvara_rais %>% 
+    indpat_rais() %>% 
+    indsimples_rais() %>% 
+    grinstrucao_rais() %>% 
+    genero_rais_2012 %>% 
+    nacionalidad_rais() %>% 
+    tpdefic_rais() %>% 
+    raca_cor_rais() %>% 
+    tamestab_rais() %>% 
+    tipoestbl_rais() %>% 
+    commareplace_rais() %>% 
+    natjuridica_rais() %>% 
+    causafast_rais() %>% 
+    diainiafmesiniaf_rais() %>% 
+    dtadmissao_rais_2011() %>% 
+    as_tibble() %>% 
+    rais_assertclass() %>%
+    reorder_rais_2011() 
+  
+}
