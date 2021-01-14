3a2db96d7326dd583138a4ca174ae47f69534177
emgriggs <66389050+emgriggs@users.noreply.github.com>
2021-01-14 10:13:21 -0800

    Create clean_rais_2003.R

---
+++ code/clean_rais_2003.R
@@ -0,0 +1,33 @@
+clean_rais_2003 <- function(rais_data) {
+  rais_data %>% 
+    as.data.table() %>% 
+    rename_rais_2005() %>% 
+    make_rais_lazy() %>% 
+    dropTIPOESTBID_rais() %>% 
+    destring_rais_2003() %>% 
+    trim_rais() %>% 
+    CPF_rais() %>% 
+    identificad_rais() %>% 
+    radiccnpj_rais() %>% 
+    empem3112_rais %>% 
+    tpvinculo_rais() %>% 
+    causadesli_rais() %>% 
+    ocupacao94_rais_2005() %>% 
+    tipoadm_rais() %>% 
+    tiposal_rais() %>% 
+    indalvara_rais %>% 
+    indpat_rais() %>% 
+    indsimples_rais() %>% 
+    grinstrucao_rais() %>% 
+    dtadmissao_rais() %>% 
+    dtnascimento_rais() %>% 
+    genero_rais_2004() %>% 
+    nacionalidad_rais() %>% 
+    raca_cor_rais() %>% 
+    tamestab_rais() %>% 
+    tipoestbl_rais() %>% 
+    commareplace_rais() %>% 
+    natjuridica_rais() %>% 
+    as_tibble() %>% 
+    assertclass_rais() 
+}
