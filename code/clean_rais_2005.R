b8485bd8d07dd5024cd0bf6c2a71b5ec36f59cac
emgriggs <66389050+emgriggs@users.noreply.github.com>
2021-01-14 08:25:38 -0800

    Update clean_rais_2005.R

--- code/clean_rais_2005.R
+++ code/clean_rais_2005.R
@@ -29,8 +29,6 @@ clean_rais_2005 <- function(rais_data) {
     tipoestbl_rais() %>% 
     commareplace_rais() %>% 
     natjuridica_rais() %>% 
-    dtnascimento_rais() %>% 
-    dtadmissao_rais() %>% 
     as_tibble() %>% 
     assertclass_rais() 
 }
