87cee0c37c4870c45934d66f4f7ec6aafdc235d6
emgriggs <66389050+emgriggs@users.noreply.github.com>
2021-01-21 10:13:31 -0800

    Create clean_rais_2007.R

---
+++ code/clean_rais_2007.R
@@ -0,0 +1,39 @@
+##needs Latin-1 encoding
+clean_rais_2007 <- function(rais_data) {
+  rais_data %>% 
+    as.data.table() %>% 
+    rename_rais_2007() %>% 
+    make_rais_lazy() %>% 
+    dropTIPOESTBID_rais() %>% 
+    destring_rais_2007() %>%
+    trim_rais() %>%  
+    CPF_rais() %>% 
+    identificad_rais() %>% 
+    radiccnpj_rais() %>% 
+    empem3112_rais %>% 
+    tpvinculo_rais() %>% 
+    causadesli_rais() %>% 
+    diadesli_rais() %>% 
+    ocupacao94_rais_2005() %>% 
+    tipoadm_rais() %>% 
+    ocup2002_rais() %>%
+    tiposal_rais() %>% 
+    indalvara_rais %>% 
+    indpat_rais() %>% 
+    indsimples_rais() %>% 
+    grinstrucao_rais() %>% 
+    dtadmissao_rais() %>% 
+    dtnascimento_rais() %>% 
+    genero_rais_2005() %>% 
+    nacionalidad_rais() %>% 
+    raca_cor_rais() %>% 
+    tpdefic_rais() %>% 
+    tamestab_rais() %>% 
+    tipoestbl_rais() %>% 
+    commareplace_rais() %>% 
+    natjuridica_rais() %>% 
+    causafast_rais() %>% 
+    diainiafmesiniaf_rais() %>% 
+    as_tibble()  %>% 
+    assertclass_rais() 
+}
