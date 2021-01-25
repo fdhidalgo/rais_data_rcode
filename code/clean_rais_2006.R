b6817564d3f7c9042ceef8429a63077d68e7aa2f
emgriggs <66389050+emgriggs@users.noreply.github.com>
2021-01-25 09:23:03 -0800

    Update clean_rais_2006.R

--- code/clean_rais_2006.R
+++ code/clean_rais_2006.R
@@ -1,7 +1,7 @@
 clean_rais_2006 <- function(rais_data) {
   rais_data %>% 
     as.data.table() %>% 
-    rename_rais_2006() %>% 
+    rename_rais() %>% 
     make_rais_lazy() %>% 
     dropTIPOESTBID_rais() %>% 
     destring_rais_2006() %>% 
