ca92f86e1008e04578cc41ecad5d389473e8c38a
emgriggs <66389050+emgriggs@users.noreply.github.com>
2021-01-25 09:25:42 -0800

    Update clean_rais_2015.R

--- code/clean_rais_2015.R
+++ code/clean_rais_2015.R
@@ -1,7 +1,7 @@
 clean_rais_2015 <- function(rais_data) {
   rais_data %>% 
     as.data.table() %>% 
-    rename_rais_2015() %>% 
+    rename_rais() %>% 
     make_rais_lazy() %>% 
     destring_rais_2015() %>%
     trim_rais() %>% 
