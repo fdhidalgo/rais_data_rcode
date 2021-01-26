6f0cc761507cb2c236d552b3c0c0a7c8a029c683
emgriggs <66389050+emgriggs@users.noreply.github.com>
2021-01-26 12:04:23 -0800

    Create clean_rais_1995.R

---
+++ code/clean_rais_1995.R
@@ -0,0 +1,24 @@
+clean_rais_1995 <- function(rais_data) {
+  rais_data %>% 
+    as.data.table() %>% 
+    rename_rais() %>% 
+    make_rais_lazy() %>%
+    destring_rais_1998() %>%
+    trim_rais_2001() %>% 
+    identificad_rais() %>%
+    radiccnpj_rais() %>%
+    empem3112_rais %>%
+    tpvinculo_rais() %>% 
+    causadesli_rais() %>%
+    tipoadm_rais() %>%
+    ocupacao94_rais_2005() %>%
+    grinstrucao_rais() %>%
+    genero_rais_2004() %>%
+    nacionalidad_rais() %>%
+    tamestab_rais() %>%
+    tipoestbl_rais() %>%
+    commareplace_rais_1998() %>%
+    ibgesubsetor_rais() %>% 
+    as_tibble()  %>% 
+    assertclass_rais()
+}
